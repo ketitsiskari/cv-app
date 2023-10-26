df93e95aa78518d20e9ac2ceca8c74d0e3f509db
keti <ketitsiskaridze@gmail.com>
2023-10-26 22:51:02 +0400

    impruve skills

--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -64,7 +64,7 @@ export const Skills = () => {
           icon={faPenToSquare}
           text="Open edit"
           id="skills"
-          className="skills__button"
+          className={`skills__button`}
           onClick={handleOpen}
         />
   
